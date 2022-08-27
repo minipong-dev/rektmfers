e77c70dfc15224faf5e7499782b81aedc01c08c6
minipong-dev <60086046+minipong-dev@users.noreply.github.com>
2022-08-27 00:21:55 -0700

    Add files via upload

--- nft-minting-app-main/src/index.js
+++ nft-minting-app-main/src/index.js
@@ -1,17 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./OLD App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 import reportWebVitals from "./reportWebVitals";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 import "./styles/reset.css";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+        <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
